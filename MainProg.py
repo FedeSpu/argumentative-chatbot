7b79d96eb99ba3b550d5768f6dff2ff295bba408
FedeSpu <federico.spurio@studio.unibo.it>
2020-03-19 18:55:50 +0100

    Initial commit

---
+++ MainProg.py
@@ -0,0 +1,10 @@
+import subprocess as sub
+from pathlib import Path
+
+margotBash = 'home/Scrivania/Tesi/predictor/run_margot.sh'
+sample = 'home/Scrivania/Tesi/predictor/sample.txt'
+outDir = 'home/Scrivania/Tesi/predictor/output'
+print("Start")
+# sub.Popen(['bash', margotBash, sample, outDir])
+home = Path.
+sub.Popen('/home/Scrivania/Tesi/tesi_py/prova.sh')
